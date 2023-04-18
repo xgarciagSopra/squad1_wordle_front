3611c49d2229074b0ebeefbad0df6a584fdd1d0d
MONTOJO Juan Jose <juan-jose.valles@ext.soprasteria.com>
2023-04-18 15:53:35 +0200

    F1_ResultBox[FRONT]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultBoxComponent } from './result-box/result-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultBoxComponent
   ],
   imports: [
     BrowserModule,
