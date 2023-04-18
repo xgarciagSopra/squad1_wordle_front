0b69a010bbdf2ff2ba26883636474642dbe27ceb
Pau Bigorra <pau-miguel.bigorra@ext.soprasteria.com>
2023-04-18 15:52:20 +0200

    test de prueba

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,6 @@
+/// <reference types="cypress" />
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visitUrl('http://localhost:4200/')
+    })
+  })
---
+++ cypress/support/commands.js
@@ -0,0 +1,7 @@
+Cypress.Commands.add('visitUrl', (label) => {
+    cy.visit(label)
+})
+
+Cypress.Commands.add('checkTitleText',() => {
+    cy.get
+})
