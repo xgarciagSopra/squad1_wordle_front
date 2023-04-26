c5f032c0c2c09807e7851cc9f20c790dc09fcdc4
Xavier García <100375613+elxaavoo@users.noreply.github.com>
2023-04-26 10:13:30 +0200

    fix() Change sendKey position

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -12,28 +12,21 @@ export class KeyboardComponent {
 
   firstKeyBoardRow = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
   secondKeyBoardRow = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ'];
-  thirdKeyBoardRow = ['⌫', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '➜'];
+  thirdKeyBoardRow = ['⌫', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+  sendKey = '➜';
 
-  disableSendKey(key: string): boolean {
-    return (key === '➜' && !this.disableKeys);
+  disableSendKey(): boolean {
+    return !this.disableKeys;
   }
 
-  disableAllKeys(): boolean{
-    return (this.roundFound === false)
+  disableAllKeys(): boolean {
+    return this.roundFound === false;
   }
 
-  comproveAllKeysToDisable(key: string){
-    return (this.disableAllKeys() || this.disableSendKey(key))
-  }
+  // comproveAllKeysToDisable(key: string){
+  //     return this.disableSendKey(key) || this.disableAllKeys
 
-  keyAllStyles(key: string){
-    return (this.roundFound === false)?'keyDisabled':'key';
-  }
-
-
-  keyStyles(key: string) {
-    return key === '➜' && !this.disableKeys ? 'keyDisabled' : 'key';
-  }
+  // }
 
   pressKeyValue(letter: string) {
     this.pressedKeyValue.emit(letter);
