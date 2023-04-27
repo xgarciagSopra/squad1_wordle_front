66be8272205bd33105919cf042930d32f17d7b57
Xavier García <100375613+elxaavoo@users.noreply.github.com>
2023-04-27 09:19:36 +0200

    feat() New Colors save

--- src/app/components/result-box/result-box.component.ts
+++ src/app/components/result-box/result-box.component.ts
@@ -9,12 +9,28 @@ export class ResultBoxComponent {
   @Input() word = ''
   @Input() splitWord: string[] = [];
   @Input() borderResultStyles!: boolean;
+  @Input() keyBoxStyle: string = '';
   @Input() firstRound = false;
 
-  borderStyles(): string{
+  boxStyles(): string{
     if (!this.firstRound) return '';
     return this.borderResultStyles
-      ? 'border-success'
-      : 'border-danger';
+      ? 'hit'
+      : 'parcialHit';
+  }
+
+  newBoxStyles(): string{
+    switch(this.keyBoxStyle){
+      case (''):
+        return this.keyBoxStyle;
+      case ('hit'):
+        return this.keyBoxStyle;
+      case ('parcialHit'):
+        return this.keyBoxStyle;  
+      case ('fail'):
+        return this.keyBoxStyle;  
+      default:
+        return '';
+    }
   }
 }
