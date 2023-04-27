e986194e6a187f31ac94339efd8054b9740cd44a
Xavier García <100375613+elxaavoo@users.noreply.github.com>
2023-04-27 08:16:21 +0200

    test() make test

--- src/app/components/game-center/game-center.component.spec.ts
+++ src/app/components/game-center/game-center.component.spec.ts
@@ -1,16 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameCenterComponent } from './game-center.component';
+import { MatDialog } from '@angular/material/dialog';
+import { GuessWordService } from 'src/app/services/guess-word.service';
+import { ToastrService } from 'ngx-toastr';
+import { ResultBoxComponent } from '../result-box/result-box.component';
+import { KeyboardComponent } from '../keyboard/keyboard.component';
+
+const guessWordService = jasmine.createSpyObj('GuessWordService', [
+  'newRound',
+  'checkWord',
+]);
+
+const toastrService = jasmine.createSpyObj('ToastrService', [
+  'warning',
+]);
 
 describe('GameCenterComponent', () => {
   let component: GameCenterComponent;
   let fixture: ComponentFixture<GameCenterComponent>;
-
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameCenterComponent ]
-    })
-    .compileComponents();
+      declarations: [GameCenterComponent, ResultBoxComponent, KeyboardComponent],
+      providers: [
+        { provide: MatDialog, useValue: {} },
+        { provide: GuessWordService, useValue: guessWordService },
+        { provide: ToastrService, useValue: toastrService },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameCenterComponent);
     component = fixture.componentInstance;
@@ -20,4 +38,8 @@ describe('GameCenterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should check', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
--- src/app/services/guess-word.service.spec.ts
+++ src/app/services/guess-word.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GuessWordService } from './guess-word.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('GuessWordService', () => {
   let service: GuessWordService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler],
+    });
     service = TestBed.inject(GuessWordService);
   });
 
