85a859eba37930e3e0ac34e274e9d37bd6689c04
Xavier García <100375613+elxaavoo@users.noreply.github.com>
2023-05-02 12:11:54 +0200

    save()

--- src/app/components/win-round-dialog/win-round-dialog.component.ts
+++ src/app/components/win-round-dialog/win-round-dialog.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-win-round-dialog',
   templateUrl: './win-round-dialog.component.html',
-  styleUrls: ['./win-round-dialog.component.scss']
+  styleUrls: ['./win-round-dialog.component.scss'],
 })
-export class WinRoundDialogComponent {
-
-}
+export class WinRoundDialogComponent {}
