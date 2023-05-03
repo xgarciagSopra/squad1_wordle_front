8efb705d0e142f65e119ac8a07f046d98c7e8095
Pau Bigorra <pau-miguel.bigorra@ext.soprasteria.com>
2023-05-03 11:53:54 +0200

    Create test

---
+++ cypress/e2e/endGame.5.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+
+describe('#5 End Game', () => {
+
+    it('Show win game alert', () => {
+        
+    });
+    it('Block all page actions', () => {
+        
+    });
+});
