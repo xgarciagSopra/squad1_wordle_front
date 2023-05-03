7971c85e3e2f4527e2b12ba60a0acc38c7321626
juanjovSopra <juan-jose.valles@ext.soprasteria.com>
2023-05-03 09:30:18 +0200

    fix(T63):Resolver issue error

--- src/app/components/error-round-dialog/error-round-dialog.component.ts
+++ src/app/components/error-round-dialog/error-round-dialog.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-error-round-dialog',
   templateUrl: './error-round-dialog.component.html',
-  styleUrls: ['./error-round-dialog.component.scss']
+  styleUrls: ['./error-round-dialog.component.scss'],
 })
-export class ErrorRoundDialogComponent {
-
-}
+export class ErrorRoundDialogComponent {}
