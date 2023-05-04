224842188ec57ae56fbaf62b5ffeb482cf2d1e82
Xavier García <100375613+elxaavoo@users.noreply.github.com>
2023-05-04 11:07:07 +0200

    save() mit

--- src/app/components/attemp-rows/attemp-rows.component.ts
+++ src/app/components/attemp-rows/attemp-rows.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Attempt } from 'src/app/interfaces/attempt.interface';
 
 @Component({
   selector: 'app-attemp-rows',
   templateUrl: './attemp-rows.component.html',
-  styleUrls: ['./attemp-rows.component.scss']
+  styleUrls: ['./attemp-rows.component.scss'],
 })
 export class AttempRowsComponent {
-
+  @Input() attempsRows!: Attempt[];
 }
