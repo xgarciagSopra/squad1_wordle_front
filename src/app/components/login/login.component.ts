3a01cb5a306c62bad84ed153c624ca9585ede73d
juanjovSopra <juan-jose.valles@ext.soprasteria.com>
2023-05-10 14:30:23 +0200

    fix(T74):clean code plus

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,26 +1,7 @@
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-} from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { fromByteArray } from 'base64-js';
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(
-      control?.invalid &&
-      (control.dirty || control.touched || isSubmitted)
-    );
-  }
-}
+import { MyErrorStateMatcher } from 'src/app/helpers/errorStateMatcher';
 
 const alphaNumeric = '^[a-zA-Z0-9]*$';
 
@@ -58,21 +39,25 @@ export class LoginComponent {
   }
 
   getUserError(): string {
-    const user = this.form.controls.user;
-    if (user.errors!['required']) return 'Usuario requerido';
-    if (user.errors!['minlength'])
+    const userErrors = this.form.controls['user'].errors;
+    if (!userErrors) return '';
+    if (userErrors['required']) {
+      return 'Usuario requerido';
+    }
+    if (userErrors['minlength'])
       return 'El Usuario debe tener mínimo 4 caracteres';
-    if (user.errors!['pattern'])
+    if (userErrors['pattern'])
       return 'El usuario no permite caracteres especiales';
     return 'Por favor inserta un usuario válido';
   }
 
   getPasswordError(): string {
-    const password = this.form.controls.password;
-    if (password.errors!['required']) return 'Contraseña requerida';
-    if (password.errors!['minlength'])
+    const passwordErrors = this.form.controls['password'].errors;
+    if (!passwordErrors) return '';
+    if (passwordErrors['required']) return 'Contraseña requerida';
+    if (passwordErrors['minlength'])
       return 'La contraseña debe tener mínimo 6 caracteres';
-    if (password.errors!['maxlength'])
+    if (passwordErrors['maxlength'])
       return 'La contraseña debe tener máximo 12 caracteres';
     return 'Por favor inserta una contraseña válida';
   }
